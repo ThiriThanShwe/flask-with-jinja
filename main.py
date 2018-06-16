00e9b6a797c193c8a4060fcf89bd75a1fca77833
ThiriThanShwe <thirithanshwe12396@gmail.com>
2018-06-15 21:04:19 -0700

    added index.html and files in static folder

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 myapp = Flask(__name__)
 
 @myapp.route('/')
-def index():
-    return "Hello from Flask"
+def hello():
+    return render_template("index.html") 
 
 if __name__ == "__main__":
     myapp.run(debug=True)
